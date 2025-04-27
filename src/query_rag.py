7f4763a5fe1caec8a9c22131df7ee4453dedd74a
Kumar-1919 <chspkr111@gmail.com>
2025-04-27 18:32:33 +0530

    Code modified

--- src/query_rag.py
+++ src/query_rag.py
@@ -1,5 +1,8 @@
 from langchain_community.llms import Ollama
 from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from tqdm import tqdm
+import time
 
 USE_FAKE_EMBEDDINGS = False  # 🛠️ Change if needed
 
@@ -9,14 +12,41 @@ def query_rag(vectordb, query):
     else: 
         llm = Ollama(
             model="llama3",  # OR 'mistral', 'qwen', etc
-            base_url="http://localhost:11434"
-        )
+           # base_url="http://localhost:11434"
+            temperature = 0.2,
+            num_ctx = 2048,
+            top_p = 0.9
+            )
+
+        retriever = vectordb.as_retriever(search_kwargs={"k":5})
+
+
+        prompt=PromptTemplate.from_template("""
+        Answer the question based only on the context below.
+        Question:{question}
+        Context:
+        {context}
+    
+        If you don't know the answer, say 'I don't know'.
+        """ )
 
         qa_chain = RetrievalQA.from_chain_type(
             llm=llm,
+            retriever=retriever,
             chain_type="stuff",
-            retriever=vectordb.as_retriever()
+            chain_type_kwargs={"prompt": prompt}
         )
 
+        print("Fetching best matching documents......")
+        docs=retriever.get_relevant_documents(query)
+
+        print("Generating the answer (using Llama3)....")
+        
+        for percent in tqdm(range(100), desc="Progress",ncols=80,bar_format='{l_bar}{bar} |{n_fmt}%'):
+            time.sleep(0.02)
+        
+
         result = qa_chain.run(query)
+        print("\n Answer is Ready!")
+
         return result
